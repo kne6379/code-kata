c8b918db77b22dfb23f8a01846a2869e1348bfbd
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-07-11 02:00:26 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    return arr.reduce((acc, cur) => {
+        const recursive = (min, max) =>{
+          return (min % max) === 0 ? max : recursive(max, min % max);
+        }
+        let max = 0;
+        return acc*cur / recursive(acc,cur);
+    });
+}
