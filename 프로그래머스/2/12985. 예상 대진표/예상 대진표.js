c646c23c7f4ae99cee726950bfcd619b721bb273
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-08-06 09:39:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,15 +1,10 @@
 function solution(n, a, b) {
   let round = 0;
 
-  const recursion = (a, b, round) => {
-    if (a !== b) {
-      a = Math.ceil(a / 2);
-      b = Math.ceil(b / 2);
-      round++;
-      return recursion(a, b, round);
-    }
-    return round;
-  };
-  const answer = recursion(a, b, round);
-  return answer;
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    round++;
+  }
+  return round;
 }
