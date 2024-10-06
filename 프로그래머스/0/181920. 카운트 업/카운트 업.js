e6a26ea2cfda9f579f075fe06b6cfd866c755e19
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-10-06 19:26:14 +0900

    [level 0] Title: 카운트 업, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,13 +1,6 @@
 function solution(start_num, end_num) {
-    const result = [];
-
-    const recursiveHelper = (current) => {
-        if(current <= end_num){
-            result.push(current)
-            return recursiveHelper(current+1);
-        }else{
-            return result
-            }
-        }
-    return recursiveHelper(start_num);
+    if (start_num > end_num) {
+        return [];
     }
+    return [start_num, ...solution(start_num + 1, end_num)];
+}
