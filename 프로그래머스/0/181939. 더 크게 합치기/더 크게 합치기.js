ed69a1fc590e50971ff70d07cf9d4f9f3776637b
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-10-07 18:59:03 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,4 +1,4 @@
 function solution(a, b) {
-    var answer = Math.max(Number(String(a)+String(b)),Number(String(b)+String(a)))
-    return answer;
+  var answer = Math.max(Number(`${a}${b}`),Number(`${b}${a}`));
+  return answer;
 }
