58f0463322b41a1c55cfea7248d86e986371311d
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-10-10 11:59:45 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,1 +1,14 @@
-const solution = (chicken) => parseInt((chicken-1) / 9)
+function solution(chicken, result = 0) {
+
+    if(chicken >= 10) {
+
+        result += Math.floor(chicken/10);
+
+        chicken = Math.floor(chicken/10) + chicken % 10;
+        
+        return solution(chicken, result)
+    }
+
+    return result;
+}
+
