781610b43d1da266825c3e5bbf19ffcea6026a27
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-11-07 20:48:32 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,5 +1,3 @@
 function solution(myString) {
-    return myString
-        .replaceAll("a", "A")               // 모든 "a"를 "A"로 변환
-        .replace(/[A-Z]/g, (char) => char === "A" ? "A" : char.toLowerCase()); // 나머지 대문자는 소문자로 변환
+    return myString.toLowerCase().replaceAll("a", "A");
 }
