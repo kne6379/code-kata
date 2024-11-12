146586e242818dc78ed7b7691b999a405d167376
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-11-12 22:01:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,17 +1,1 @@
-function solution(A, B) {
-    let temp = A.split("");
-    let temp2 = ""
-    const length = temp.length;
-    
-    for(let i=0; i < length ; i++){
-        if(A===B) return i;
-        temp.unshift(temp[length-1]);
-        temp.pop();
-        temp2 = temp.join("");
-        if(temp2 === B){
-            return i+1;
-        }
-        
-    }
-    return -1;
-}
+let solution=(a,b)=>(b+b).indexOf(a)
