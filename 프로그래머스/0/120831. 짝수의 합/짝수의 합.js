608599efc217d42562bb376cdc06c1d18b91ee9d
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-11-16 16:13:20 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,8 +1,7 @@
 function solution(n) {
     let result = 0;
-    for(let i=0; i <= n; i+=2){
+    for(let i=2; i <= n; i+=2){
         result += i;
-        console.log(result)
     }
     return result;
 }
