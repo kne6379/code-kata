a33b3d91316fa203f9f74f3822faa0a4f66ceb68
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-12-02 23:31:17 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    let discount = 0;
+    if(price >= 500000){
+        discount = 0.2;
+    }else if(price >= 300000){
+        discount = 0.1;
+    }else if(price >= 100000){
+        discount = 0.05;
+    }
+    return Math.floor(price * (1 - discount));
+}
