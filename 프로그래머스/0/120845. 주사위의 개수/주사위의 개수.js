a6d7c55d7d8db3cc363c5b656b1ac8eeeb0a69ad
Noeul Kim <128128422+kne6379@users.noreply.github.com>
2024-12-14 21:43:11 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,6 +1,5 @@
 function solution(box, n) {
     const divide = (number) => Math.floor(number / n);
-    const [wide, depth, height] = [divide(box[0]), divide(box[1]), divide(box[2])];
-    return wide * depth * height;
+    return divide(box[0]) * divide(box[1]) * divide(box[2]);
 }
 
